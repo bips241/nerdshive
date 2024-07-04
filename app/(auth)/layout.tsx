c45c5a3492ec7758dae67ef27357673afc5f3ba6
@bips241 <biplabmal626@gmail.com>
2024-07-04 22:42:28 +0530

    form-updated

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,29 @@
-function AuthLayout({ children }: { children: React.ReactNode }) {
-    return <div className="grid place-items-center h-screen">{children}</div>;
-  }
-  
-  export default AuthLayout;
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import '../../app/globals.css';
+import AuthProvider from '../../context/AuthProvider';
+import { Toaster } from '@/components/ui/toaster';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'True Feedback',
+  description: 'Real feedback from real people.',
+};
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default async function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en" >
+      <AuthProvider>
+        <body className={inter.className}>
+          {children}
+          <Toaster />
+        </body>
+      </AuthProvider>
+    </html>
+  );
+}
