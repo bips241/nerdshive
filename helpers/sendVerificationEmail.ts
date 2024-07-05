0f6c35e5cd812e1b23c9c3a41454cc2e5c8f68ee
@bips241 <biplabmal626@gmail.com>
2024-07-05 15:13:48 +0530

    tiny change

--- helpers/sendVerificationEmail.ts
+++ helpers/sendVerificationEmail.ts
@@ -9,7 +9,7 @@ export async function sendVerificationEmail(
 ): Promise<ApiResponse> {
   try {
     await resend.emails.send({
-      from: 'nerdshive.net',
+      from: 'nerdshive.online',
       to: email,
       subject: 'Mystery Message Verification Code',
       react: VerificationEmail({ username, otp: verifyCode }),
