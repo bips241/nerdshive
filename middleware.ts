fee14f35af6c8cda8218ffe531440b659894475a
@bips241 <biplabmal626@gmail.com>
2024-07-13 22:51:53 +0530

    solved major issue in middleware causing edge runtime error resulting build failed

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,24 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { auth } from './auth';
+
+
 
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/login', '/register', '/', '/verify/:path*'],
+  matcher: ['/dashboard/:path*', '/login', '/register', '/', '/verify/:path*','/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
 
 export default async function middleware(request: NextRequest) {
-    const session = await auth();
-    const user = session?.user;
+  const currentUser = request.cookies.get('authjs.session-token')?.value;
+    
   
     const url = request.nextUrl;
 
-    console.log(`user`, user);
+    console.log(`currentUser`, currentUser);
   
 
   
   if (
-    user &&
+    currentUser &&
     (url.pathname.startsWith('/login') ||
       url.pathname.startsWith('/register') ||
       url.pathname.startsWith('/verify') ||
@@ -26,7 +27,7 @@ export default async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL('/dashboard', request.url));
   }
 
-  if (!user && url.pathname.startsWith('/dashboard')) {
+  if (!currentUser && url.pathname.startsWith('/dashboard')) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
 
